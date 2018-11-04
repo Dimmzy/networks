e0f6558ba192ddd0b300d2ebaefb369206ece269
Dima <leetsushi@gmail.com>
2018-11-04 17:58:05 +0200

    DNS server & parent done.

--- ex1/DNSParent.py
+++ ex1/DNSParent.py
@@ -2,8 +2,7 @@ import sys
 from socket import socket, AF_INET, SOCK_DGRAM
 
 
-# Need to add master server IP Address Validation
-ipList = open(str(sys.argv[4]), 'r+')
+ipList = open(str(sys.argv[4]), 'r')
 ip_dict = {}
 for line in ipList:
     line = line.strip()
@@ -11,20 +10,14 @@ for line in ipList:
     ip_dict[key] = val
 source_ip = '0.0.0.0'
 source_port = int(sys.argv[1])
-parentIP = str(sys.argv[2])
-parentPort = int(sys.argv[3])
 s = socket(AF_INET, SOCK_DGRAM)
 s.bind((source_ip,source_port))
 while True:
     data, sender_info = s.recvfrom(2048)
-    found = 0
     for address in ip_dict:
         if data == address:
             found = 1
             s.sendto(ip_dict[address], sender_info)
             break
     if not found:
-        clientSender = sender_info
-        s.sendto(data,(parentIP,parentPort))
-        data, sender_info = s.recvfrom(2048)
-        s.sendto(data, clientSender)
+        print 'Address not found in DNS registry'
--- ex1/udpCLIENT.py
+++ ex1/udpCLIENT.py
@@ -1,14 +1,14 @@
-# UDP Client Program
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
 
-from socket import socket, AF_INET, SOCK_DGRAM # מייבא מספריית הסוקט את הפונק הדרושות לפתיחת סוקט
-
-s = socket(AF_INET, SOCK_DGRAM) # יוצר סוקט UDP, בפרוטוקול ipv4
-dest_ip = '172.18.36.119' # כתובת האייפי של שרת היעד
-dest_port = 12345 # הפורט שעליו שרת היעד מאזין
-msg = raw_input("Message to send: ") # קולט את ההודעה שהמשתמש רוצה לשלוח
-while not msg == 'quit': # כל עוד המשתמש לא שלח הודעה יציאה, ממשיך בתהליך שליחת הודעות
-    s.sendto(msg,(dest_ip,dest_port)) # 
+s = socket(AF_INET, SOCK_DGRAM) 
+dest_ip = str(sys.argv[1]) 
+dest_port = int(sys.argv[2])
+msg = raw_input("Find IP of: ") 
+while not msg == 'quit': 
+    s.sendto(msg,(dest_ip,dest_port)) 
     data, sender_info = s.recvfrom(2048)
-    print "Server sent: ", data
-    msg = raw_input("Message to send: ")
+    print "IP Address: ", data
+    msg = raw_input("Find IP of: ")
 s.close()
+
