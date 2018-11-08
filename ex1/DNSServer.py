9184747dc602b6ad72667e00fd1a49b2d1fad2a4
Dima <leetsushi@gmail.com>
2018-11-08 17:33:13 +0200

    Ex1 Complete, Summary PDF is left to do

--- ex1/DNSServer.py
+++ ex1/DNSServer.py
@@ -1,37 +1,66 @@
+"""
+A DNS Server program that receives address queries and returns their
+corresponding IP addresses to the client.
+"""
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
-def makeDict(ipFile)
-
-# Need to add master server IP Address Validation
-ipList = open(str(sys.argv[4]), 'a+')
-ipDictionary = makeDict(ipList)
-source_ip = '0.0.0.0'
-source_port = int(sys.argv[1])
-parentIP = str(sys.argv[2])
-parentPort = int(sys.argv[3])
-s = socket(AF_INET, SOCK_DGRAM)
-s.bind((source_ip,source_port))
-while True:
-    data, sender_info = s.recvfrom(2048)
-    found = 0
-    for address in ipDictionary:
-        if data == address:
-            found = 1
-            s.sendto(ipDictionary[address], sender_info)
-            break
-    if not found:
-        dataAdd = data
-        clientSender = sender_info
-        s.sendto(data,(parentIP,parentPort))
-        data, sender_info = s.recvfrom(2048)
-        s.sendto(data, clientSender)
-        ipList.write( '\n' + dataAdd + ',' + data)
 
 
-def makeDict(ipFile):
+def make_dict(ip_file):
+    """
+    Creates a dictionary from the given IP file for easier access
+    and comparions.
+    # Args:
+    ip_file (file): A text file containing addresses and their IP's.
+    # Returns:
+    dictionary: A dictionary with the address as key and IP as value.
+    """
     ip_dict = {}
-    for line in ipList:
-        line = line.strip()
-        (key, val) = line.rsplit(',')
-        ip_dict[key] = val
+    for line in ip_file:
+        if line != '\n':
+            line = line.strip()
+            (key, val) = line.rsplit(',')
+            ip_dict[key] = val
     return ip_dict
+
+def main():
+    """
+    Starts up a socket and runs a DNS server that provides answers to IP
+    address queries from clients, if no match is found in the provided ips.txt
+    file, passes the query to it's parent server. (defined in main args)
+    """
+    ip_list = open(str(sys.argv[4]), 'r+')
+    ip_dict = make_dict(ip_list) # We create a dictionary using make_dict func
+    source_ip = '0.0.0.0' # Receives IP/Port information from main args
+    source_port = int(sys.argv[1])
+    parent_ip = str(sys.argv[2])
+    parent_port = int(sys.argv[3])
+    sock = socket(AF_INET, SOCK_DGRAM) # Binds its IP and Port to a new socket
+    sock.bind((source_ip, source_port))
+    while True: # Runs the server indefinitly
+        data, sender_info = sock.recvfrom(2048) # Recevies queries from clients
+        found = 0 # flag used to check if the query was answered
+        # We perform a loop that compares the address in question with all
+        # addresses given in teh ips file, if a match is found, return the IP
+        # to the client and finish.
+        for address in ip_dict:
+            if data == address:
+                found = 1
+                sock.sendto(ip_dict[address], sender_info)
+                break
+        # If the address is not found in the dictionary, query the parent DNS
+        # server and return the answer to the client.
+        if not found:
+            data_address = data
+            client_sender = sender_info
+            sock.sendto(data, (parent_ip, parent_port))
+            data, sender_info = sock.recvfrom(2048)
+            sock.sendto(data, client_sender)
+            # The program also "remembers" the address and IP given, and writes
+            # it to the ips.txt file and the dictionary.
+            ip_list.write(data_address + ',' + data + '\n')
+            ip_list.flush()
+            ip_dict[data_address] = data
+
+if __name__ == "__main__":
+    main()
--- ex1/udpCLIENT.py
+++ ex1/udpCLIENT.py
@@ -1,14 +1,26 @@
+"""
+A UDP Client program. Sends a query to a DNS server with the input address
+and receives the adderss's IP from the DNS server.
+"""
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
 
-s = socket(AF_INET, SOCK_DGRAM) 
-dest_ip = str(sys.argv[1]) 
-dest_port = int(sys.argv[2])
-msg = raw_input("Find IP of: ") 
-while not msg == 'quit': 
-    s.sendto(msg,(dest_ip,dest_port)) 
-    data, sender_info = s.recvfrom(2048)
-    print "IP Address: ", data
+def main():
+    """
+    Creates a socket and sends the user input to the server specified in the
+    program arguments.
+    If the message that was sent is quit, close the socket and finish.
+    """
+    sock = socket(AF_INET, SOCK_DGRAM)
+    dest_ip = str(sys.argv[1])
+    dest_port = int(sys.argv[2])
     msg = raw_input("Find IP of: ")
-s.close()
+    while not msg == 'quit':
+        sock.sendto(msg, (dest_ip, dest_port))
+        data, _ = sock.recvfrom(2048) # _ for sender_info since we don't use it
+        print "IP Address: ", data
+        msg = raw_input("Find IP of: ")
+    sock.close()
 
+if __name__ == "__main__":
+    main()
