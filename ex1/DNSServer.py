356f14d8309d3b52250762dc2cd61bec7fd48dc1
Dima <leetsushi@gmail.com>
2018-11-08 15:17:39 +0200

    E3 Updates

--- ex1/DNSServer.py
+++ ex1/DNSServer.py
@@ -1,14 +1,10 @@
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
-
+def makeDict(ipFile)
 
 # Need to add master server IP Address Validation
-ipList = open(str(sys.argv[4]), 'r+')
-ip_dict = {}
-for line in ipList:
-    line = line.strip()
-    (key, val) = line.rsplit(',')
-    ip_dict[key] = val
+ipList = open(str(sys.argv[4]), 'a+')
+ipDictionary = makeDict(ipList)
 source_ip = '0.0.0.0'
 source_port = int(sys.argv[1])
 parentIP = str(sys.argv[2])
@@ -18,13 +14,24 @@ s.bind((source_ip,source_port))
 while True:
     data, sender_info = s.recvfrom(2048)
     found = 0
-    for address in ip_dict:
+    for address in ipDictionary:
         if data == address:
             found = 1
-            s.sendto(ip_dict[address], sender_info)
+            s.sendto(ipDictionary[address], sender_info)
             break
     if not found:
+        dataAdd = data
         clientSender = sender_info
         s.sendto(data,(parentIP,parentPort))
         data, sender_info = s.recvfrom(2048)
         s.sendto(data, clientSender)
+        ipList.write( '\n' + dataAdd + ',' + data)
+
+
+def makeDict(ipFile):
+    ip_dict = {}
+    for line in ipList:
+        line = line.strip()
+        (key, val) = line.rsplit(',')
+        ip_dict[key] = val
+    return ip_dict
